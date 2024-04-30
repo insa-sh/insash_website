856e159629f4862b91076a7154bd6e6534e4dc08
TuperWar2.0 <louison.bednarowicz@outlook.com>
2024-04-30 20:39:34 +0200

    feat:header - Premiers pas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   // variables à exporter vers la page utilisables en ecrivant {{ nomVariable }} sur la page
