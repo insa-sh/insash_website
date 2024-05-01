272bab1a6b02a526f8b931aad6adf8882bff31e1
TuperWar2.0 <louison.bednarowicz@outlook.com>
2024-05-01 02:21:47 +0200

    feat: logo updated + Fontawesome icon kit imported with JS

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -18,4 +18,23 @@ export class HeaderComponent {
     { link: 'team', title: 'Équipe' },
     { link: 'contact', title: 'Contact' }
   ];
+  constructor() { 
+    this.loadScripts(); 
+  } 
+ 
+  // Method to dynamically load JavaScript 
+  loadScripts() { 
+ 
+    // This array contains all the files/CDNs 
+    const dynamicScripts = [ 
+       'https://kit.fontawesome.com/1416c49ef4.js'
+    ]; 
+    for (let i = 0; i < dynamicScripts.length; i++) { 
+      const node = document.createElement('script'); 
+      node.src = dynamicScripts[i]; 
+      node.type = 'text/javascript'; 
+      node.async = false; 
+      document.getElementsByTagName('head')[0].appendChild(node); 
+    } 
+  }
 }
