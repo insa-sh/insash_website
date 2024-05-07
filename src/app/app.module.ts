1b85ffeb973c6844e0d9faedebc0f572da60d9c4
Amg <armandessetcheou@gmail.com>
2024-05-08 00:44:14 +0200

    feat rejoindreLeClub component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FooterComponent } from './footer/footer.component';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { RejoindreLeClubComponent } from './rejoindre-le-club/rejoindre-le-club.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
+    RejoindreLeClubComponent,
   ],
   imports: [
     BrowserModule
