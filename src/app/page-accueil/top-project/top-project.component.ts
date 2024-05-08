c59d5fdf4a28889e1f9da0ef96879a998b50cb83
DYSTOpyy <bcd.saltel@gmail.com>
2024-05-08 22:55:30 +0200

    feat(top-projets): template des projets ajoutés, top-projets avec affichage dynamique, retour à l'img plutôt que les div

--- src/app/page-accueil/top-project/top-project.component.ts
+++ src/app/page-accueil/top-project/top-project.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { Project } from "src/app/models/project";
 
 @Component({
-  selector: 'app-top-project',
-  templateUrl: './top-project.component.html',
-  styleUrls: ['./top-project.component.css']
+  selector: "app-top-project",
+  templateUrl: "./top-project.component.html",
+  styleUrls: ["./top-project.component.css"],
 })
 export class TopProjectComponent {
+  readonly baseUrl = "/assets/images/projets";
 
+  topProjects: Project[] = [
+    {
+      id: 487,
+      name: "Guerre des Carrés",
+      authors: ["Maxime", "Baptiste"],
+      tags: ["Jeu", "Web", "JavaScript"],
+      photo: `${this.baseUrl}/guerre_des_carre.png`,
+      date: new Date(2023, 5),
+      icon: true,
+    },
+    {
+      id: 42,
+      name: "Ecclogy (Nuit de l'info 2023)",
+      authors: ["Maxime", "Baptiste", "Louison", "Maël", "Clément", "Nathan"],
+      tags: ["Challenge", "Web", "Django"],
+      photo: `${this.baseUrl}/ecclogy.png`,
+      date: new Date(2023, 12),
+      icon: true,
+    },
+    {
+      id: 777,
+      name: "Site web ./insa.sh",
+      authors: ["Louison", "Baptiste"],
+      tags: ["Web", "Angular"],
+      photo: `${this.baseUrl}/web_site.png`,
+      date: new Date(2024, 5),
+      icon: true,
+    },
+  ];
 }
