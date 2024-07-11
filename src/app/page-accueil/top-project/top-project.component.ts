b95ef462ce29f38b0e1318d78f51cda2ef7a71ca
B <bcd.saltel@gmail.com>
2024-07-11 21:22:48 +0200

    feat: ajout des top-projects sur la page projets + modification du CSS

--- src/app/page-accueil/top-project/top-project.component.ts
+++ src/app/page-accueil/top-project/top-project.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, Input } from "@angular/core";
 import { Project } from "src/app/models/project";
 import { DocumentService, SortingByDate } from "src/app/interaction-backend/document.service";
 import { DocumentAndAuthor } from "src/app/interaction-backend/document-and-author";
@@ -14,6 +14,7 @@ export class TopProjectComponent {
 
   public topProjects: DocumentAndAuthor[] = [];
   public authors: Member[] = [];
+  @Input() numberOfProjects: number = 3; 
 
   constructor(private documentService: DocumentService) {
 
@@ -21,15 +22,12 @@ export class TopProjectComponent {
 
   fetchTopProjects() {
 
-
-
-    this.documentService.getProject([], "", "", "", SortingByDate.Asc, 3)
+    this.documentService.getProject([], "", "", "", SortingByDate.Asc, this.numberOfProjects)
       .subscribe(
         (data: any) => {
-          
+
           this.topProjects = data.map((project: any) => {
-            //
-            // console.log(this.authors);
+
             return new DocumentAndAuthor(
               new Document(
                 project.title,
@@ -45,53 +43,40 @@ export class TopProjectComponent {
                 project.slug,
                 project.is_image_icon,
 
-
-
               ),
               []
             );
           });
-          console.log(this.topProjects);
           this.topProjects.forEach(projectAndAuthor => {
-            console.log(projectAndAuthor);
-             this.documentService.getProjectAuthor(projectAndAuthor.document.slug)
-          .subscribe(
-            (data: any) => {
-              projectAndAuthor.authors = data.map((membre: any) => {
-  
-                return new Member(
-                  membre.firstname,
-                  membre.lastname,
-                  membre.year,
-                  membre.role,
-                  membre.website,
-                  membre.mail,
-                  membre.image_address,
-                  membre.linkedin,
-                  membre.github,
-                  membre.citation,
-                  membre.surname
-                )
-              }
-  
-              
-  
-              ); 
-              
-            }
-          );
-  
-  
+            this.documentService.getProjectAuthor(projectAndAuthor.document.slug)
+              .subscribe(
+                (data: any) => {
+                  projectAndAuthor.authors = data.map((membre: any) => {
+
+                    return new Member(
+                      membre.firstname,
+                      membre.lastname,
+                      membre.year,
+                      membre.role,
+                      membre.website,
+                      membre.mail,
+                      membre.image_address,
+                      membre.linkedin,
+                      membre.github,
+                      membre.citation,
+                      membre.surname
+                    )
+                  }
+
+                  );
+
+                }
+              );
+
           });
-          
+
         })
 
-        console.log("je suis deuxième");
-        
-
-
-
-        
   }
 
 
