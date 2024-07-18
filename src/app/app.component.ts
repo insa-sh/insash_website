fe508cef61ba284bf6bd83e34c22d295345f47b8
TuperWar2.0 <80649077+BillyTheSecond@users.noreply.github.com>
2024-07-18 23:14:09 +0200

    feat: le scroll de la page revient à 0 (en haut)  au changement de page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,27 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { FooterComponent } from "./footer/footer.component";
 import { HeaderComponent } from "./header/header.component";
 import { CommonModule } from "@angular/common";
-import { RouterLink, RouterLinkActive, RouterOutlet } from "@angular/router";
+import { Router, RouterLink, RouterLinkActive, RouterOutlet, NavigationEnd } from "@angular/router";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   // variables à exporter vers la page utilisables en ecrivant {{ nomVariable }} sur la page
   title = "Site Web du Club Info";
+
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        window.scrollTo(0, 0);
+      }
+    });
+  }
+
+  
 }
