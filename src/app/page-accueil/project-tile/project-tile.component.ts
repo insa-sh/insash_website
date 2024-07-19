c64877c93b99ad2fc9385d129c6aa14162460319
B <bcd.saltel@gmail.com>
2024-07-19 16:03:50 +0200

    feat: max 3 tags pour les project-tile des top-project (sans compter le futur tag "type")

--- src/app/page-accueil/project-tile/project-tile.component.ts
+++ src/app/page-accueil/project-tile/project-tile.component.ts
@@ -2,6 +2,8 @@ import { Component, Input } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { DocumentAndAuthor } from "src/app/models/document-and-author";
 
+
+
 @Component({
   selector: "app-project-tile",
   templateUrl: "./project-tile.component.html",
@@ -9,4 +11,10 @@ import { DocumentAndAuthor } from "src/app/models/document-and-author";
 })
 export class ProjectTileComponent {
   @Input() project!: DocumentAndAuthor;
+  public MAX_TAGS: number = 3;
+
+  getMaxTags(max: number) {
+    return this.project.document.tags.slice(0, max);
+  }
+
 }
