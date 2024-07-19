7f095d1df8e4f2e2f3b9cfdfd60b2fcf6e0283b0
B <bcd.saltel@gmail.com>
2024-07-19 16:08:31 +0200

    fix: suppression des queryParams dans les résolveurs (inutile et dangereux au final)

--- src/app/interaction-backend/resolver/document.resolver.ts
+++ src/app/interaction-backend/resolver/document.resolver.ts
@@ -7,32 +7,14 @@ import { DocumentAndAuthor } from "src/app/models/document-and-author";
 import { Member } from "../member";
 
 
-const createDocumentResolver = (documentType?: DocumentType, nbr? : number): ResolveFn<DocumentAndAuthor> => {
+const createDocumentResolver = (documentType: DocumentType, nbr? : number): ResolveFn<DocumentAndAuthor> => {
     return (route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<DocumentAndAuthor> => {
 
-        let tags = route.queryParamMap.getAll('tag');
-        let search = route.queryParamMap.get('search');
-        if (!search) search = "";
-        let uuid = route.queryParamMap.get('uuid');
-        if (!uuid) uuid = "";
-        let slug = route.queryParamMap.get('slug');
-        if (!slug) slug = "";
-        let years = route.queryParamMap.getAll('year');
-        let authors = route.queryParamMap.getAll('author');
-        let sort = route.queryParamMap.get('sort') as SortingBy;
         if (nbr == null) {
-            let queryNbr = route.queryParamMap.get('nbr');
-            if (queryNbr) {
-                nbr = +queryNbr;
-            }
-            
+            nbr = 0;
         } 
-        
-        if (documentType == null) {
-            documentType = route.queryParamMap.get('type') as DocumentType;
-        }
 
-        return inject(DocumentService).getDocument(documentType = documentType, tags = tags, search = search, uuid = uuid, slug = slug, years = years, sort = sort, authors = authors, nbr = nbr);
+        return inject(DocumentService).getDocument(documentType = documentType, [], "", "", "", [], SortingBy.dateAsc, [], nbr = nbr);
     };
 };
 
