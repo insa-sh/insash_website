2f940b37e635e9717fcb4b72f0c0d409e84896e7
Prismey <prismey@murena.io>
2024-09-28 15:17:57 +0200

    Fixed some bugs

--- insash_website_frontend/src/app/interaction-backend/document.service.ts
+++ insash_website_frontend/src/app/interaction-backend/document.service.ts
@@ -13,7 +13,7 @@ export enum SortingBy {
   nameDesc = "name_desc",
 }
 
-const BASE_URL: string = "http://localhost:8080/"
+const BASE_URL: string = "http://localhost/api/"
 
 @Injectable({
   providedIn: 'root'
