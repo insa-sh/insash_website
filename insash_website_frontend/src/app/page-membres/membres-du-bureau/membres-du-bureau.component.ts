8e0763ec8c8e0c190c2b674211166490ea4a5f03
DYSTOpyy <bcd.saltel@gmail.com>
2024-10-02 22:11:31 +0200

    fix: Réorganiser les membres du bureau close #58

--- insash_website_frontend/src/app/page-membres/membres-du-bureau/membres-du-bureau.component.ts
+++ insash_website_frontend/src/app/page-membres/membres-du-bureau/membres-du-bureau.component.ts
@@ -1,14 +1,32 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input } from "@angular/core";
 import { Member } from "src/app/models/member";
 
-
 @Component({
-  selector: 'app-membres-du-bureau',
-  templateUrl: './membres-du-bureau.component.html',
-  styleUrls: ['./membres-du-bureau.component.css']
+  selector: "app-membres-du-bureau",
+  templateUrl: "./membres-du-bureau.component.html",
+  styleUrls: ["./membres-du-bureau.component.css"],
 })
 export class MembresDuBureauComponent {
-
   // tableaux d'éléments de type Member (dit 'interface')
   @Input() membres!: Member[]; // ! -> la variable membres est obligatoire et ne sera jamais null ou undefined
+
+  president: Member | undefined;
+  secretaire: Member | undefined;
+  tresorier: Member | undefined;
+  responsableCommunication: Member | undefined;
+
+  ngOnInit() {
+    this.president = this.membres.find(
+      (m) => m.role.String.toLowerCase() === "président"
+    );
+    this.secretaire = this.membres.find(
+      (m) => m.role.String.toLowerCase() === "secrétaire"
+    );
+    this.tresorier = this.membres.find(
+      (m) => m.role.String.toLowerCase() === "trésorier"
+    );
+    this.responsableCommunication = this.membres.find(
+      (m) => m.role.String.toLowerCase() === "responsable communication"
+    );
+  }
 }
--- insash_website_frontend/src/app/page-membres/page-membres.component.ts
+++ insash_website_frontend/src/app/page-membres/page-membres.component.ts
@@ -1,104 +1,30 @@
-import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
 import { Member } from "src/app/models/member";
-import { DocumentService } from '../interaction-backend/document.service';
-
+import { DocumentService } from "../interaction-backend/document.service";
 
 @Component({
-  selector: 'app-page-membres',
-  templateUrl: './page-membres.component.html',
-  styleUrls: ['./page-membres.component.css']
+  selector: "app-page-membres",
+  templateUrl: "./page-membres.component.html",
+  styleUrls: ["./page-membres.component.css"],
 })
 export class PageMembresComponent {
-
   public membres: Member[] = [];
 
-  constructor(private route: ActivatedRoute, private documentService: DocumentService) {
-
-  }
+  constructor(
+    private route: ActivatedRoute,
+    private documentService: DocumentService
+  ) {}
 
   fetchMembers() {
-    this.route.data.subscribe(
-      (data) => {
-
-        if (data['members'] != null) {
-
-          this.membres = data['members'];
-        }
-
-      })
-
-
+    this.route.data.subscribe((data) => {
+      if (data["members"] != null) {
+        this.membres = data["members"];
+      }
+    });
   }
 
   ngOnInit() {
     this.fetchMembers();
   }
-
-
-  // membres: Member[] = [
-  //   {
-  //     id: 0,
-  //     name: "Sss",
-  //     last_name: "Sss",
-  //     pseudo: "ssssss",
-  //     role_tag: "bigboss",
-  //     role: "Président",
-  //     bureau_comment: "// si tu fais planter le site t’auras affaire à lui",
-  //     photo: "",
-  //     email: "president@insash.org",
-  //     email_visible: true,
-  //   },
-  //   {
-  //     id: 1,
-  //     name: "Rrr",
-  //     last_name: "Ccc",
-  //     pseudo: "rrrccc",
-  //     role_tag: "moneyguy",
-  //     role: "Trésorier",
-  //     bureau_comment: "// Money Money Money - Abba",
-  //     photo: "",
-  //     email: "tresorier@insash.org",
-  //     email_visible: true,
-  //   },
-  //   {
-  //     id: 2,
-  //     name: "Aaa",
-  //     last_name: "Lll",
-  //     pseudo: "aaalll",
-  //     role_tag: "secretaire",
-  //     role: "Secrétaire",
-  //     bureau_comment: undefined,
-  //     photo: "",
-  //     email: "secretaire@insash.org",
-  //     email_visible: false,
-  //   },
-  //   {
-  //     id: 3,
-  //     name: "Bbb",
-  //     last_name: "Bbb",
-  //     pseudo: "bbbbbb",
-  //     role_tag: "instagrameur",
-  //     role: "Respo Communication & Graphismes",
-  //     bureau_comment: undefined,
-  //     photo: "",
-  //     email: "communication@insash.org",
-  //     email_visible: true,
-  //   },
-  //   {
-  //     id: 4,
-  //     name: "TTT",
-  //     last_name: "Ccc",
-  //     pseudo: "tttccc",
-  //     role_tag: "membre",
-  //     role: "Membre ",
-  //     bureau_comment: undefined,
-  //     photo: "",
-  //     email: "",
-  //     email_visible: false,
-  //   }
-
-  // ];
-
-   
 }
