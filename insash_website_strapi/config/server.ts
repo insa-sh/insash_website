a4c43beb24091c6f797dadd044728b38c8137e18
DYSTOpyy <bcd.saltel@gmail.com>
2025-05-09 11:23:01 +0200

    fix : revert APP_URL changes

--- insash_website_strapi/config/server.ts
+++ insash_website_strapi/config/server.ts
@@ -1,7 +1,7 @@
 export default ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
-  url: env("APP_URL", "http://localhost:1337"), // Utilisation de la variable d'environnement APP_URL
+  url: "http://localhost/insash-website-data",
   app: {
     keys: env.array("APP_KEYS"),
   },
